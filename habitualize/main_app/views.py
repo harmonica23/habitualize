25524cd36782c182f85a69515333f1c743f0d879
ninaraynor <ninaraynor@gmail.com>
2024-01-18 15:14:58 -0500

    connect to neon

--- habitualize/main_app/views.py
+++ habitualize/main_app/views.py
@@ -4,16 +4,17 @@ from .models import Habit
 
 
 # Create your views here.
+def habits_list(request):
+  habits = Habit.objects
+  return render(request, 'habits/list.html', {
+    'habits': habits
+  })
+
 def home(request):
   return render(request, 'home.html')
 
 def calendar(request):
   return render(request, 'calendar.html')
-
-def habits_list(request):
-  return render(request, 'habits/list.html', {
-    'habits': habits
-  })
 
 def habits_detail(request, habit_id):
   habit = Habit.objects.get(id=habit_id)
@@ -24,6 +25,7 @@ def habits_detail(request, habit_id):
 class HabitCreate(CreateView):
   model = Habit
   fields = '__all__'
+  success_url='/habits'
 
   def form_valid(self, form):
     form.instance.user = self.request.user

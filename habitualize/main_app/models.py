904cd3ae7e185abbe13a701fe40350484d810728
ninaraynor <ninaraynor@gmail.com>
2024-01-18 16:47:20 -0500

    login/logout functionality

--- habitualize/main_app/models.py
+++ habitualize/main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Habit(models.Model):
   name = models.CharField(max_length=100)
   streak = models.IntegerField()
   make_or_break = models.CharField(max_length=100)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return f'{self.name} ({self.id})'

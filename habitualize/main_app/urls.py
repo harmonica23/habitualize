9c254810c78c3310d4d1a13c5d32b5c59d5ba122
emilyTheseond <esegundo02@gmail.com>
2024-01-18 14:41:33 -0600

    render view all

--- habitualize/main_app/urls.py
+++ habitualize/main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('habits/', views.habits_list, name='list'),
+  path('habits/', views.habits_index, name='index'),
   path('habits/<int:habit_id>/', views.habits_detail, name='detail'),
   path('habits/create/', views.HabitCreate.as_view(), name='create'),
   path('calendar/', views.calendar, name='calendar'),

9de24b97335b8eeb4bce7ec3e7e52cffadfcc652
emilyTheseond <esegundo02@gmail.com>
2024-01-23 10:01:26 -0600

    journal view

--- habitualize/main_app/models.py
+++ habitualize/main_app/models.py
@@ -35,6 +35,12 @@ class Journal(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   entry = models.CharField(max_length=500)
   habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+  date = models.DateField('Journal Date')
+  class Meta:
+    ordering = ['-date']
+
+  def __str__(self):
+      return f"{self.user.username}'s Journal Entry"
 
 class Mood(models.Model):
   mood = object
